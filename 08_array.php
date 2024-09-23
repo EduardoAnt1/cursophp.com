f69026cebff219ae06bd58d8b83b2c589fa4a271
Eduardo Antunes <eduardoantunes.2001@gmail.com>
2024-09-22 21:38:30 -0300

    edit aula 08

--- 08_array.php
+++ 08_array.php
@@ -1,4 +1,41 @@
 <?php
 //Arrays
-$carros = array("BMW","Veloster","Hillux");
-echo $carros[0];
+$carros = array(1=>"BMW",2=>"Veloster",3=>"Hillux");
+$carros[] = 'Amarok';
+$carros[10] = "Camaro";
+print_r($carros);
+
+echo "<br>";
+echo "<hr>";
+
+$motos = array();
+$motos[] = "Yamaha";
+$motos[]= "Honda";
+$motos[5] = "Suzuki";
+echo $motos[1];
+
+echo "<br>";
+echo "<hr>";
+
+
+$clientes = ["Eduardo","Luis","Felipe"];
+print_r($clientes);
+
+echo "<br>";
+echo "<hr>";
+
+//count
+echo 'Temos '.count($carros).' carros';
+echo "<br>";
+echo 'Temos '.count($motos).' motos';
+echo "<br>";
+echo 'Temos '.count($clientes).' clientes';
+echo "<br>";
+
+echo "<hr>";
+
+//For each client
+echo 'Clientes:';
+foreach ($clientes as $nome){
+    echo "<br>".$nome;
+}
